dddd722fee6d208cb7c27ea02d91c53cdfe3c9fb
chomnr <zeljkovranjes@outlook.com>
2023-12-21 10:26:44 -0500

    added more configuration.

--- src/app/services/iam/auth.rs
+++ src/app/services/iam/auth.rs
@@ -1,6 +1,14 @@
 use core::fmt;
 use std::env;
 
+use oauth2::{
+    basic::BasicClient, AuthUrl, ClientId, ClientSecret, RedirectUrl, RevocationUrl, TokenUrl,
+};
+
+///
+/// Prerequisites
+///
+
 /// Specifies the available authentication types for the system.
 ///
 /// Variants:
@@ -34,25 +42,80 @@ impl fmt::Display for OAuthProvider {
 /// - `token_url`: URL to retrieve the authentication token.
 /// - `revoke_url`: URL to revoke the authentication.
 struct OAuthConfig {
-    client_id: String,
-    client_secret: String,
-    auth_url: String,
-    token_url: String,
-    revoke_url: String,
+    client_id: ClientId,
+    client_secret: ClientSecret,
+    auth_url: AuthUrl,
+    token_url: TokenUrl,
+    revoke_url: RevocationUrl,
 }
 
 impl OAuthConfig {
+    /// Creates an OAuth configuration from environment variables based on the specified provider.
+    ///
+    /// The function reads environment variables to set up the OAuth configuration for the given provider.
+    /// It expects specific variables for client ID, client secret, authentication URL, token URL,
+    /// and revocation URL to be set in the environment.
+    ///
+    /// `provider_type`: The type of OAuth provider for which to fetch the configuration.
+    ///
+    /// Example:
+    /// ```
+    /// let config = OAuthConfig::from_env(OAuthProvider::Discord);
+    /// ```
     pub fn from_env(provider_type: OAuthProvider) -> Self {
         let name = provider_type.to_string();
         Self {
-            client_id: env::var(format!("{}_CLIENT_ID", name)).expect(&name),
-            client_secret: env::var(format!("{}_CLIENT_SECRET", name)).expect(&name),
-            auth_url: env::var(format!("{}_AUTH_URL", name)).expect(&name),
-            token_url: env::var(format!("{}_TOKEN_URL", name)).expect(&name),
-            revoke_url: env::var(format!("{}_REVOCATION_URL", name)).expect(&name),
+            client_id: ClientId::new(env::var(format!("{}_CLIENT_ID", name)).expect(&name)),
+            client_secret: ClientSecret::new(
+                env::var(format!("{}_CLIENT_SECRET", name)).expect(&name),
+            ),
+            auth_url: AuthUrl::new(env::var(format!("{}_AUTH_URL", name)).expect(&name))
+                .expect("Error: unable to get auth url"),
+            token_url: TokenUrl::new(env::var(format!("{}_TOKEN_URL", name)).expect(&name))
+                .expect("Error: unable to get token url"),
+            revoke_url: RevocationUrl::new(
+                env::var(format!("{}_REVOCATION_URL", name)).expect(&name),
+            )
+            .expect("Error: unable to get revocation url"),
         }
     }
 }
+
+struct AuthClient {
+    client: BasicClient,
+}
+
+impl AuthClient {
+    /// Constructs a new OAuth client configuration based on the given provider.
+    ///
+    /// This function creates a new OAuth client with settings obtained from environment variables.
+    /// The configuration includes client ID, client secret, authentication URL, token URL,
+    /// and revocation URL specific to the OAuth provider.
+    ///
+    /// `provider`: The OAuth provider (e.g., Discord) to configure the client for.
+    ///
+    /// Example:
+    /// ```
+    /// let oau
+    pub fn new(provider: OAuthProvider) -> Self {
+        let config = OAuthConfig::from_env(provider);
+        let redirect_url = RedirectUrl::new(env::var("OAUTH2_REDIRECT_URL").expect("REDIRECT URL"))
+            .expect("Unable to get redirect url.");
+        let client = BasicClient::new(
+            config.client_id,
+            Some(config.client_secret),
+            config.auth_url,
+            Some(config.token_url),
+        )
+        .set_redirect_uri(redirect_url)
+        .set_revocation_uri(config.revoke_url);
+        Self { client }
+    }
+}
+
+///
+/// Routing
+///
 
 /*
 struct AuthClient;
