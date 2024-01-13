b089161c02a51082d298db8957b265f517435696
chomnr <zeljkovranjes@outlook.com>
2024-01-13 14:46:24 -0500

    added permissionbuilder.

--- src/app/platform/iam/permission/model.rs
+++ src/app/platform/iam/permission/model.rs
@@ -1,8 +1,50 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Permission {
     permission_id: String,
     permission_name: String,
-    permission_key: String
+    permission_key: String,
+}
+
+impl Permission {
+    pub fn builder() -> PermissionBuilder {
+        PermissionBuilder::new()
+    }
+}
+
+#[derive(Default)]
+pub struct PermissionBuilder {
+    permission_id: String,
+    permission_name: String,
+    permission_key: String,
+}
+
+impl PermissionBuilder {
+    pub fn new() -> PermissionBuilder {
+        PermissionBuilder {
+            permission_id: Uuid::new_v4().to_string(),
+            permission_name: String::default(),
+            permission_key: String::default(),
+        }
+    }
+
+    pub fn permission_name(mut self, permission_name: &str) -> PermissionBuilder {
+        self.permission_name = String::from(permission_name);
+        self
+    }
+
+    pub fn permission_key(mut self, permission_key: &str) -> PermissionBuilder {
+        self.permission_key = String::from(permission_key);
+        self
+    }
+
+    pub fn build(self) -> Permission {
+        Permission {
+            permission_id: self.permission_id,
+            permission_name: self.permission_name,
+            permission_key: self.permission_key,
+        }
+    }
 }
--- src/app/platform/iam/role/model.rs
+++ src/app/platform/iam/role/model.rs
@@ -1,7 +1,41 @@
 use serde::{Serialize, Deserialize};
+use uuid::Uuid;
 
 #[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Role {
     role_id: String,
     role_name: String
+}
+
+impl Role {
+    pub fn builder() -> RoleBuilder {
+        RoleBuilder::new()
+    }
+}
+
+#[derive(Default)]
+pub struct RoleBuilder {
+    role_id: String,
+    role_name: String
+}
+
+impl RoleBuilder {
+    pub fn new() -> RoleBuilder {
+        RoleBuilder {
+            role_id: Uuid::new_v4().to_string(),
+            role_name: String::default(),
+        }
+    }
+
+    pub fn role_name(mut self, role_name: &str) -> RoleBuilder {
+        self.role_name = String::from(role_name);
+        self
+    }
+
+    pub fn build(self) -> Role {
+        Role {
+            role_id: self.role_id,
+            role_name: self.role_name
+        }
+    }
 }
