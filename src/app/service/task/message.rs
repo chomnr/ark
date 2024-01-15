95833b0c37c24a2742a33e56bfa4a4bfb8067310
chomnr <zeljkovranjes@outlook.com>
2024-01-15 12:34:36 -0500

    setting up necessary information for a task.

---
+++ src/app/service/task/message.rs
@@ -0,0 +1,85 @@
+use nanoid::nanoid;
+use serde::{Deserialize, Serialize};
+
+/// Represents the status of a task.
+///
+/// This enum is used to indicate whether a task has been completed successfully
+/// or has failed. It's a part of the task response to signify the outcome of the task.
+#[derive(Serialize, Deserialize)]
+pub enum TaskStatus {
+    /// Indicates that the task was completed successfully.
+    Completed,
+
+    /// Indicates that the task has failed.
+    Failed,
+}
+
+/// A request structure for a task.
+///
+/// This struct is used to encapsulate the details of a task request. It includes
+/// the unique identifier of the task and the payload which contains the details
+/// or data required to perform the task.
+#[derive(Serialize, Deserialize)]
+pub struct TaskRequest {
+    /// A unique identifier for the task.
+    pub task_id: String,
+
+    /// The payload of the task, containing the details or data for the task.
+    pub task_payload: String,
+}
+
+impl TaskRequest {
+    /// Composes a new task request with a given payload.
+    pub fn compose_request<T: for<'a> Deserialize<'a> + Serialize>(task_payload: T) -> Self {
+        Self {
+            task_id: format!("task-{}", nanoid!(7)),
+            task_payload: serde_json::to_string(&task_payload).unwrap(),
+        }
+    }
+}
+
+/// A generic response structure for a task.
+///
+/// This struct is used to represent the response of a task. It is generic over `T`,
+/// allowing for flexibility in the type of result that the task returns. It includes
+/// the unique task identifier, the result of the task, and the status of the task.
+#[derive(Serialize, Deserialize)]
+pub struct TaskResponse {
+    /// The unique identifier of the task.
+    pub task_id: String,
+
+    /// The result of the task. The type of this field is a String to accommodate
+    /// various types of task results.
+    pub task_result: String,
+
+    /// The status of the task, indicating whether it was completed successfully
+    /// or failed.
+    pub task_status: TaskStatus,
+
+    /// The errors that occur when the task_status fails when processing the given 
+    /// task.
+    pub task_error: Vec<String>
+}
+
+impl TaskResponse {
+    /// Composes a TaskResponse from a TaskRequest sparingly.
+    pub fn compose_response<'a, T: Deserialize<'a> + Serialize>(
+        request: TaskRequest,
+        task_status: TaskStatus,
+        task_result: T,
+        task_error: Vec<String>
+    ) -> Self {
+        Self {
+            task_id: request.task_id,
+            task_result: serde_json::to_string(&task_result).unwrap(),
+            task_status,
+            task_error,
+        }
+    }
+}
+
+/*
+pub fn test() {
+    TaskResponse::compose_response::<Permission>(todo!(), todo!(), todo!());
+}
+*/
---
+++ src/app/service/task/mod.rs
@@ -0,0 +1,41 @@
+//
+// Task System
+//
+// +---------------------+
+// | User Sends Request  |
+// +----------+----------+
+//            |
+//            v
+// +----------+----------+       +--------------------------+
+// | Inbound Channel     |       | Process Request          |
+// | Receives Request    | ----> | and Check for Errors     |
+// +---------------------+       +----------+---------------+
+//                                          |
+//                               +----------+-----------+
+//                               |                      |
+//                       +-------v------+       +-------v------+
+//                       | Process      |       | Handle       |
+//                       | Successfully |       | Errors       |
+//                       +-------+------+       +-------+------+
+//                               |                      |
+//                               v                      v
+//                       +-------+------+       +-------+------+
+//                       | Outbound     |       | Outbound     |
+//                       | Channel      |       | Channel      |
+//                       | Sends Result |       | Sends Result |
+//                       | to User      |       | to User      |
+//                       +--------------+       +--------------+
+// 
+
+use crossbeam_channel::{Receiver, Sender, unbounded};
+use once_cell::sync::Lazy;
+
+use self::message::{TaskRequest, TaskResponse};
+
+pub mod message;
+
+/// Receives tasks and processing them.
+static INBOUND: Lazy<(Sender<TaskRequest>, Receiver<TaskRequest>)> = Lazy::new(|| unbounded());
+
+/// Receives results from tasks and sends them back to the origin point.
+static OUTBOUND: Lazy<(Sender<TaskResponse>, Receiver<TaskResponse>)> = Lazy::new(|| unbounded());
