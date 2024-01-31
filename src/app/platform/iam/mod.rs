5a871f1ce20c1b77fd835258b00a71c9ab39265e
chomnr <zeljkovranjes@outlook.com>
2024-01-31 14:45:12 -0500

    setting up user session.

--- src/app/platform/iam/mod.rs
+++ src/app/platform/iam/mod.rs
@@ -1,3 +1,4 @@
 pub mod permission;
 pub mod role;
+pub mod session;
 pub mod user;
---
+++ src/app/platform/iam/session/task.rs
@@ -0,0 +1,5 @@
+pub struct SessionTaskHandler;
+
+// redis is used instead of postgres;
+// user is cached
+// the session is not cached.. (even though it is in the database.)
