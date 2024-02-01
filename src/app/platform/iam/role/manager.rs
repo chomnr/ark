0a09fe9efe187abc82cbc2c8817e3ba7000599c0
chomnr <zeljkovranjes@outlook.com>
2024-01-31 19:32:25 -0500

    added cascade of session_create.

--- src/app/platform/iam/role/manager.rs
+++ src/app/platform/iam/role/manager.rs
@@ -1,5 +1,3 @@
-use std::process::id;
-
 use crate::app::service::task::{
     error::TaskResult,
     manager::TaskManager,
--- src/app/platform/iam/session/task.rs
+++ src/app/platform/iam/session/task.rs
@@ -1,13 +1,19 @@
 use axum::async_trait;
 
-use crate::app::{database::{postgres::PostgresDatabase, redis::RedisDatabase}, service::task::{message::{TaskRequest, TaskResponse}, TaskHandler}};
+use crate::app::{database::{postgres::PostgresDatabase, redis::RedisDatabase}, service::task::{error::TaskError, message::{TaskRequest, TaskResponse}, TaskHandler}};
 
 pub struct SessionTaskHandler;
 
 #[async_trait]
 impl TaskHandler<RedisDatabase> for SessionTaskHandler {
     async fn handle(redis: &RedisDatabase, task_request: TaskRequest) -> TaskResponse {
-        todo!()
+        if task_request.task_action.eq("session_create") {
+            todo!()
+        }
+        return TaskResponse::throw_failed_response(
+            task_request,
+            vec![TaskError::FailedToFindAction.to_string()],
+        );
     }
 }
 
