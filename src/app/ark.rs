5637bc447105386a87494fda50657679ea5389da
chomnr <zeljkovranjes@outlook.com>
2024-02-10 00:27:49 -0500

    typo.

--- src/app/ark.rs
+++ src/app/ark.rs
@@ -179,9 +179,9 @@ impl ArkServer {
         PermissionManager::preload_permission_cache().unwrap();
         RoleManager::preload_role_cache().unwrap();
 
-        let test: User = UserManager::get_user("23980417-6ac7-436a-9031-4d7296f7c13b").unwrap();
+        //let test: User = UserManager::get_user("e63cd0ab-a7e7-46c5-8417-2b2c71b63bb1").unwrap();
 
-        println!("{:?}", test.info.created_at);
+        //println!("{:?}", test.info.created_at);
         //SessionManager::create_session("3b9cd87a-5228-4168-9a56-2923883b2885").unwrap();
         //let user = User::builder()
         //.verified(false)
